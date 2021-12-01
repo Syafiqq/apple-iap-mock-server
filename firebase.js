bfb5bc906b915375fe95723155d536c2f9d8d2e4
msyafiqq <id.muhammad.syafiq@gmail.com>
2021-12-01 19:18:40 +0700

    update firebase auth

--- firebase.js
+++ firebase.js
@@ -2,8 +2,10 @@ const firebase = require('firebase/app');
 const config = require('./config')
 const { Storage } = require('@google-cloud/storage');
 require('firebase/database');
+require('firebase/auth');
 
 const firebaseApp = firebase.initializeApp(config.firebase)
+firebase.auth().signInAnonymously()
 const storage = new Storage({ credentials: config.googleCloud.privateKey })
 const bucket = storage.bucket(config.firebase.storageBucket)
 
