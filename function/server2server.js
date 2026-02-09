924741334009fdc3a33b86c204119d3a6050d78d
msyafiqq <id.muhammad.syafiq@gmail.com>
2026-02-09 19:05:59 +0700

    Dev: disable insert rtd

--- function/server2server.js
+++ function/server2server.js
@@ -4,7 +4,7 @@ const storageDebug = require('../service/storage/debug')
 const saveTransaction = async s2sObj => {
     const now = new Date();
     // Start Debug Phase
-    await databaseDebug.insertRawS2S(s2sObj, now)
+    // await databaseDebug.insertRawS2S(s2sObj, now)
     await storageDebug.insertRawS2S(s2sObj, now)
     // End Debug Phase
 }
